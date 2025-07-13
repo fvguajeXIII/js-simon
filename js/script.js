94df600d4aea9373e4ed74af8aeefd8daf018933
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-13 21:43:59 +0200

    Recupero elementi dal DOM

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Elementi del DOM
+const numbersList = document.getElementById(`numbers-list`);
+const countdown = document.getElementById(`countdown`);
+const answersForm = document.getElementById(`answer-form`);
+const inputGroup = document.getElementById(`input-group`);
+const message = document.getElementById(`message`);
+const instructions = document.getElementById(`instructions`);
