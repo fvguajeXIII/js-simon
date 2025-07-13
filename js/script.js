e945d1b0b55d80a2f86a2df27b6f0ce3d5497caa
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-13 21:51:51 +0200

    Generazione dei numeri e mostrare a video

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const answersForm = document.getElementById(`answer-form`);
 const inputGroup = document.getElementById(`input-group`);
 const message = document.getElementById(`message`);
 const instructions = document.getElementById(`instructions`);
+
+//Genera 5 numeri casuali unici tra 1 e 50
+function generateRandomNumbers(count, min, max){
+  const numbers = new Set();
+  while (numbers.size < count) {
+    numbers.add(Math.floor(Math.random() * (max - min + 1)) + min)
+  }
+  return Array.from(numbers);
+}
+
+const numbersToGuess = generateRandomNumbers (5, 1, 50);
+
+//Mostra i numeri
+numbersList.innerHTML = numbersToGuess
+.map(num => `<li class="fs-1 fw-bold">${num}</li>`)
+.join(``);
