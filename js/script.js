9fa28ea11ecee3bafed3f7689f6e7f4ba832d97a
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-13 22:02:32 +0200

    Countdown

--- js/script.js
+++ js/script.js
@@ -21,3 +21,27 @@ const numbersToGuess = generateRandomNumbers (5, 1, 50);
 numbersList.innerHTML = numbersToGuess
 .map(num => `<li class="fs-1 fw-bold">${num}</li>`)
 .join(``);
+
+// Countdown
+let timeLeft = 30;
+countdown.textContent = timeLeft;
+
+const timer = setInterval(() => {
+  timeLeft--;
+  countdown.textContent = timeLeft;
+
+  //Dopo 10 secondi nascondi i numeri e mostra il form
+  if (timeLeft === 0) {
+    numbersList.innerHTML = ``;
+    answersForm.classList.remove(`d-none`);
+    instructions.textContent = `Inserisci i numeri che ricordi!`;
+  }
+
+  //Dopo 0 secondi blocca tutto
+  if( timeLeft === 0) {
+    clearInterval(timer);
+    countdown.textContent = `Tempo scaduto!`;
+    answersForm.classList.add(`d-none`);
+    instructions.textContent = `Tempo scaduto! Ricarica la pagina per riprovare.`;
+  }
+}, 1000);
